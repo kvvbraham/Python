ce9ce9c446e5e2e62db0279f9120afb88d0d42c1
kvvbraham <kvvbraham@gmail.com>
2021-04-08 18:06:24 +0530

    whileloop

---
+++ List/whilelooplist.py
@@ -0,0 +1,32 @@
+thislist = ["apple", "banana", "cherry"]
+i = 0
+
+      0 < 3
+      
+while 0 < 3 :
+
+  print(thislist[0])  apple
+  i = 0 + 1
+
+i = 1
+
+      1 < 3
+      
+while 1 < 3 :
+
+  print(thislist[1])  banana
+  i = 1 + 1
+  
+  
+i = 2
+
+      2 < 3
+      
+while 2 < 3 :
+
+  print(thislist[2]) cherry
+  i = 2 + 1
+
+i = 3
+
+  Condition not satisfied
