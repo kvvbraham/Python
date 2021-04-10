d4b8a0c348c2a64efe7b255ae0b6e3fd69de98d4
kvvbraham <kvvbraham@gmail.com>
2021-04-10 14:03:35 +0530

    pythonlists

--- List/Pythonlists.py
+++ List/Pythonlists.py
@@ -73,4 +73,3 @@ Python Collections (Arrays) :
  ->   Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
  -> Set is a collection which is unordered and unindexed. No duplicate members.
  ->   Dictionary is a collection which is ordered* and changeable. No duplicate members.
-
