5e38057f69cab92b5e4f0537998fe3a4b3d01e7f
kvvbraham <kvvbraham@gmail.com>
2021-04-10 15:14:11 +0530

    Add list

---
+++ List/Add_List_Items.py
@@ -0,0 +1,61 @@
+ Add List Items 
+ **************
+
+ Append Items :
+ ************
+
+ -> To add an item to the end of the list, use the append() method:
+
+  thislist = ["apple", "banana", "cherry"]
+
+  thislist.append("orange")
+
+  print(thislist)
+ 
+ Output : ['apple', 'banana', 'cherry', 'orange']
+
+ Insert Items :
+ ************
+
+ -> To insert a list item at a specified index, use the insert() method.
+
+ -> The insert() method inserts an item at the specified index:
+
+    thislist = ["apple", "banana", "cherry"]
+    thislist.insert(1, "orange")
+    print(thislist)
+
+   Output : ['apple', 'orange', 'banana', 'cherry']
+
+ Extend List :
+ ***********    
+
+ -> To append elements from another list to the current list, use the extend()      method.  
+
+ thislist = ["apple", "banana", "cherry"]
+ tropical = ["mango", "pineapple", "papaya"]
+
+ thislist.extend(tropical)
+
+ print(thislist)
+
+ Output : ['apple', 'banana', 'cherry', 'mango', 'pineapple', 'papaya']
+
+ Add Any Iterable :
+ ****************
+
+ -> The extend() method does not have to append lists, you can add any iterable     object (tuples, sets, dictionaries etc.).
+
+  thislist = ["apple", "banana", "cherry"]
+  thistuple = ("kiwi", "orange")
+
+  thislist.extend(thistuple)
+
+  print(thislist)
+
+ Output : ['apple', 'banana', 'cherry', 'kiwi', 'orange']
+
+
+
+ 
+
